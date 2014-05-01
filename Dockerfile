260250c100411163a1c7a1af423ad682493faf92
CoreOS Admin <core@ip-10-139-23-242.ec2.internal>
2014-05-01 23:02:41 +0000

    fix(*): working ssh agent

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 from ubuntu:12.04
 
-RUN apt-get install -y ca-certificates
+RUN apt-get install -y ca-certificates openssh-client
+# set up ssh
+ADD id_rsa /opt/id_rsa
+RUN chmod 600 /opt/id_rsa
+# add tools
 ADD updatectl /opt/updatectl
+ADD fleetctl /opt/fleetctl
 ADD update.sh /opt/update.sh
--- update.sh
+++ update.sh
@@ -6,19 +6,27 @@
 #FLEET_TUNNEL_IP
 UPDATE_SERVICE_UNIT_NAME=$1
 
+#start ssh-agent
+#start ssh-agent
+eval `ssh-agent`
+ssh-add /opt/id_rsa
+mkdir -p ~/.ssh
+touch ~/.ssh/known_hosts
+ssh-keyscan -H $FLEET_TUNNEL_IP >> ~/.ssh/known_hosts
+
 if [ -n "$UPDATE_SERVICE_OLD_VERSION" ]; then
   echo "Update available for app $UPDATE_SERVICE_APP_ID"
   echo "Updating $UPDATE_SERVICE_UNIT_NAME from  $UPDATE_SERVICE_OLD_VERSION -> $UPDATE_SERVICE_VERSION"
   # Read existing unit
-  fleetctl --tunnel $FLEET_TUNNEL_IP cat $UPDATE_SERVICE_UNIT_NAME &> $UPDATE_SERVICE_UNIT_NAME
+  /opt/fleetctl --strict-host-key-checking=false --tunnel $FLEET_TUNNEL_IP cat $UPDATE_SERVICE_UNIT_NAME &> $UPDATE_SERVICE_UNIT_NAME
   # Stop existing unit
-  fleetctl destroy $UPDATE_SERVICE_UNIT_NAME
+  /opt/fleetctl --strict-host-key-checking=false destroy $UPDATE_SERVICE_UNIT_NAME
   # Replace version
   sed -i .bak -e "s/${UPDATE_SERVICE_OLD_VERSION}/${UPDATE_SERVICE_VERSION}/g" "$UPDATE_SERVICE_UNIT_NAME"
   echo "Launching $UPDATE_SERVICE_UNIT_NAME:"
   cat $UPDATE_SERVICE_UNIT_NAME
   # Start new unit
-  fleetctl --tunnel $FLEET_TUNNEL_IP start $UPDATE_SERVICE_UNIT_NAME
+  /opt/fleetctl --strict-host-key-checking=false --tunnel $FLEET_TUNNEL_IP start $UPDATE_SERVICE_UNIT_NAME
   # Clean up files
   rm $UPDATE_SERVICE_UNIT_NAME
   rm "$UPDATE_SERVICE_UNIT_NAME.bak"
