f1b6df7eab8d9bc10294de00223f376e902e6ba6
Rob Szumski <rob@falloutweb.com>
2014-05-10 12:01:07 -0700

    feat(update.sh): fix destroy bug

--- update.sh
+++ update.sh
@@ -20,14 +20,11 @@ if [ -n "$UPDATE_SERVICE_OLD_VERSION" ]; then
   # Read existing unit
   /opt/fleetctl --strict-host-key-checking=false --tunnel $FLEET_TUNNEL_IP cat $UPDATE_SERVICE_UNIT_NAME &> $UPDATE_SERVICE_UNIT_NAME
   # Stop existing unit
-  /opt/fleetctl --strict-host-key-checking=false destroy $UPDATE_SERVICE_UNIT_NAME
+  /opt/fleetctl --strict-host-key-checking=false --tunnel $FLEET_TUNNEL_IP destroy $UPDATE_SERVICE_UNIT_NAME
   # Replace version
   sed -i .bak -e "s/${UPDATE_SERVICE_OLD_VERSION}/${UPDATE_SERVICE_VERSION}/g" "$UPDATE_SERVICE_UNIT_NAME"
   echo "Launching $UPDATE_SERVICE_UNIT_NAME:"
   cat $UPDATE_SERVICE_UNIT_NAME
   # Start new unit
   /opt/fleetctl --strict-host-key-checking=false --tunnel $FLEET_TUNNEL_IP start $UPDATE_SERVICE_UNIT_NAME
-  # Clean up files
-  rm $UPDATE_SERVICE_UNIT_NAME
-  rm "$UPDATE_SERVICE_UNIT_NAME.bak"
 fi
